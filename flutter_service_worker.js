f4e7676289addb2faa3b356545230b770dceaa20
wangjuntao <jamiesun.net@gmail.com>
2023-04-21 18:17:37 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "26991f16af0722010cd7f822f5254ec1",
-"index.html": "06ab9e628dac6be1f215ff1f73560b0f",
-"/": "06ab9e628dac6be1f215ff1f73560b0f",
-"main.dart.js": "d39ed93c1e50794966dbaae4b9b08e0f",
+"index.html": "5047190d3f9ed879345af137e9c52d0a",
+"/": "5047190d3f9ed879345af137e9c52d0a",
+"main.dart.js": "cb6f1c8bbb4f3fbcca79dc9491577a0b",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "c3118cd6d2363dc618bdb08abad9176f",
 "icons/Icon-192.png": "3dd627744c922cb1e450618075cf3406",
 "icons/Icon-maskable-192.png": "3dd627744c922cb1e450618075cf3406",
@@ -31,7 +32,11 @@ const RESOURCES = {
 "assets/assets/images/ren_avatar.png": "e31ddcfcc075aa6bbc6603baa4e31960",
 "assets/assets/images/laotchat_avatar.png": "64d9c9aa2a67d4e4f9ffaa6894442185",
 "assets/assets/images/plan.png": "dfa74b4f67f203f65d5ce693695c8ae4",
-"assets/assets/images/writing.png": "d686b3dc6afb9f0f0ecfc825ac4bea0e"
+"assets/assets/images/writing.png": "d686b3dc6afb9f0f0ecfc825ac4bea0e",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
